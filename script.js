085edd36e6a218affa3f617e9227254a8a1efa4c
Niko Felger <niko.felger@gmail.com>
2020-05-21 13:29:05 +0200

    Draw red line indicating current time

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const agendaElement = document.querySelector('.agenda');
-const hoursToDraw = Number.parseInt(agendaElement.style.getPropertyValue('--hours-to-draw'));
+const hoursToDraw = Number.parseInt(
+  agendaElement.style.getPropertyValue('--hours-to-draw'));
+const firstMinuteDrawn = Number.parseInt(
+  agendaElement.style.getPropertyValue('--day-starts-at-minute'));
+const lastMinuteDrawn = firstMinuteDrawn + hoursToDraw * 60;
 
 function drawCalendarDate(calendarDate) {
   const dateFmtOptions = { month:'short', weekday:'short' };
@@ -16,14 +20,11 @@ function drawCalendarDate(calendarDate) {
 }
 
 function drawTimeHints() {
-  const startingPoint = Number.parseInt(
-    agendaElement.style.getPropertyValue('--day-starts-at-minute'));
-  const endPoint = startingPoint + hoursToDraw * 60;
 
   function drawHours() {
-    firstFullHour = startingPoint + 60 - startingPoint % 60;
+    firstFullHour = firstMinuteDrawn + 60 - firstMinuteDrawn % 60;
 
-    for (let min = firstFullHour; min <= endPoint; min += 60) {
+    for (let min = firstFullHour; min <= lastMinuteDrawn; min += 60) {
       const hour = document.createElement('h3');
       hour.className = 'time-hint';
       hour.style.setProperty('--start-minute', min);
@@ -39,7 +40,7 @@ function drawTimeHints() {
   }
 
   function drawLinesEvery60Min(className, firstLineAfter) {
-    for (let min = startingPoint + firstLineAfter; min <= endPoint; min += 60) {
+    for (let min = firstMinuteDrawn + firstLineAfter; min <= lastMinuteDrawn; min += 60) {
       const line = document.createElement('div');
       line.className = className;
       line.style.setProperty('--start-minute', min);
@@ -48,24 +49,41 @@ function drawTimeHints() {
   }
 
   function drawMajorLines() {
-    const firstLineAfter = 60 - startingPoint % 60;
+    const firstLineAfter = 60 - firstMinuteDrawn % 60;
     drawLinesEvery60Min('rule-major', firstLineAfter)
   }
 
   function drawMinorLines() {
     let firstLineAfter;
 
-    if (startingPoint % 60 < 30) {
-      firstLineAfter = 30 - startingPoint % 30;
+    if (firstMinuteDrawn % 60 < 30) {
+      firstLineAfter = 30 - firstMinuteDrawn % 30;
     } else {
-      firstLineAfter = 60 - (startingPoint - 30) % 60;
+      firstLineAfter = 60 - (firstMinuteDrawn - 30) % 60;
     }
 
     drawLinesEvery60Min('rule-minor', firstLineAfter);
   }
 
   function drawNowRule() {
+    const nowRule = document.createElement('div');
+    nowRule.className = 'rule-now';
+    agendaElement.appendChild(nowRule);
 
+    function updateNowRulePosition() {
+      const now = new Date();
+      const nowInMinutes = now.getHours() * 60 + now.getMinutes();
+
+      if (nowInMinutes > lastMinuteDrawn) {
+        nowRule.style.display = 'none';
+        return;
+      }
+
+      nowRule.style.setProperty('--start-minute', nowInMinutes);
+
+      setTimeout(updateNowRulePosition, 60000);
+    }
+    updateNowRulePosition();
   }
 
   drawHours();
