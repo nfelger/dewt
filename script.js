cd403044deefc7c2505108cc5d7c11406ac8a4ae
Niko Felger <niko.felger@gmail.com>
2020-05-21 14:27:58 +0200

    Move day start / end configuration to JS

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 const agendaElement = document.querySelector('.agenda');
-const hoursToDraw = Number.parseInt(
-  agendaElement.style.getPropertyValue('--hours-to-draw'));
-const firstMinuteDrawn = Number.parseInt(
-  agendaElement.style.getPropertyValue('--day-starts-at-minute'));
-const lastMinuteDrawn = firstMinuteDrawn + hoursToDraw * 60;
+const hoursToDraw = 14;
+const totalMinutes = hoursToDraw * 60;
+const dayStartsAtMin = 420;
 
 function drawCalendarDate(calendarDate) {
-  const dateFmtOptions = { month:'short', weekday:'short' };
+  const dateFmtOptions = { month: 'short', weekday: 'short' };
   const [weekday, month] = new Intl.DateTimeFormat('en-US', dateFmtOptions)
     .formatToParts(calendarDate)
     .filter(({type}) => Object.keys(dateFmtOptions).includes(type))
@@ -22,14 +20,14 @@ function drawCalendarDate(calendarDate) {
 function drawTimeHints() {
 
   function drawHours() {
-    firstFullHour = firstMinuteDrawn + 60 - firstMinuteDrawn % 60;
+    firstFullHour = 60 - dayStartsAtMin % 60;
 
-    for (let min = firstFullHour; min <= lastMinuteDrawn; min += 60) {
+    for (let min = firstFullHour; min <= totalMinutes; min += 60) {
       const hour = document.createElement('h3');
       hour.className = 'time-hint';
       hour.style.setProperty('--start-minute', min);
       hour.style.setProperty('--end-minute', min + 59);
-      hour.textContent = min / 60;
+      hour.textContent = (dayStartsAtMin + min) / 60;
 
       const minute = document.createElement('sup');
       minute.textContent = '00';
@@ -40,7 +38,7 @@ function drawTimeHints() {
   }
 
   function drawLinesEvery60Min(className, firstLineAfter) {
-    for (let min = firstMinuteDrawn + firstLineAfter; min <= lastMinuteDrawn; min += 60) {
+    for (let min = firstLineAfter; min <= totalMinutes; min += 60) {
       const line = document.createElement('div');
       line.className = className;
       line.style.setProperty('--start-minute', min);
@@ -49,17 +47,17 @@ function drawTimeHints() {
   }
 
   function drawMajorLines() {
-    const firstLineAfter = 60 - firstMinuteDrawn % 60;
+    const firstLineAfter = 60 - dayStartsAtMin % 60;
     drawLinesEvery60Min('rule-major', firstLineAfter)
   }
 
   function drawMinorLines() {
     let firstLineAfter;
 
-    if (firstMinuteDrawn % 60 < 30) {
-      firstLineAfter = 30 - firstMinuteDrawn % 30;
+    if (dayStartsAtMin % 60 < 30) {
+      firstLineAfter = 30 - dayStartsAtMin % 30;
     } else {
-      firstLineAfter = 60 - (firstMinuteDrawn - 30) % 60;
+      firstLineAfter = 60 - (dayStartsAtMin - 30) % 60;
     }
 
     drawLinesEvery60Min('rule-minor', firstLineAfter);
@@ -72,9 +70,9 @@ function drawTimeHints() {
 
     function updateNowRulePosition() {
       const now = new Date();
-      const nowInMinutes = now.getHours() * 60 + now.getMinutes();
+      const nowInMinutes = now.getHours() * 60 + now.getMinutes() - dayStartsAtMin;
 
-      if (nowInMinutes > lastMinuteDrawn) {
+      if (nowInMinutes > totalMinutes) {
         nowRule.style.display = 'none';
         return;
       }
@@ -92,5 +90,6 @@ function drawTimeHints() {
   drawNowRule();
 }
 
+agendaElement.style.setProperty('--total-minutes', totalMinutes);
 drawCalendarDate(new Date());
 drawTimeHints();
