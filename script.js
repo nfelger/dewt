2d8e41def9189ae4549e0038dd4af413ae8475e4
Niko Felger <niko.felger@gmail.com>
2020-05-21 11:38:44 +0200

    Draw major (hour) lines

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const agenda = document.querySelector('.agenda');
+const hoursToDraw = Number.parseInt(agenda.style.getPropertyValue('--hours-to-draw'));
+
 function drawCalendarDate(calendarDate) {
   const dateFmtOptions = { month:'short', weekday:'short' };
   const [weekday, month] = new Intl.DateTimeFormat('en-US', dateFmtOptions)
@@ -12,4 +15,38 @@ function drawCalendarDate(calendarDate) {
   dayElements[2].textContent = month;
 }
 
+function drawTimeHints() {
+  const startingPoint = Number.parseInt(
+    agenda.style.getPropertyValue('--day-starts-at-minute'));
+  const endPoint = startingPoint + hoursToDraw * 60
+
+  function drawHours() {
+
+  }
+
+  function drawMajorLines() {
+    const startOffsetFromFullHour = startingPoint % 60;
+    for (let min = startingPoint + startOffsetFromFullHour; min <= endPoint; min += 60) {
+      const line = document.createElement('div');
+      line.className = 'rule-major';
+      line.style.setProperty('--start-minute', min);
+      agenda.appendChild(line);
+    }
+  }
+
+  function drawMinorLines() {
+
+  }
+
+  function drawNowRule() {
+
+  }
+
+  drawHours();
+  drawMajorLines();
+  drawMinorLines();
+  drawNowRule();
+}
+
 drawCalendarDate(new Date());
+drawTimeHints();
